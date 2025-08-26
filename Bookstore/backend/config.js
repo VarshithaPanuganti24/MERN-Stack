53817417625e783f318b04d18bdf2bd8cf7f0bda
Varshitha Panuganti <vpanuganti.1283@gmail.com>
2025-08-26 14:01:30 -0500

    Bookstore

---
+++ Bookstore/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+export const mongoDBURL=
+'mongodb+srv://vpanuganti1283:C6LO7t8qNVNK12Ua@bookstore-mern.ayw6mw1.mongodb.net/?retryWrites=true&w=majority&appName=BookStore-MERN';
+
+
+//please create a free database for yourself
+//this database will be deleted after tutorial
